568b03ea5ee704ac9d10a6d8af83165a96674fb5
Gyys1992 <65665662+Gyys1992@users.noreply.github.com>
2021-12-17 14:08:27 -0500

    Update main.m

--- main.m
+++ main.m
@@ -8,72 +8,34 @@ addpath ~/VCMatlab/Helper/SaliencyToolbox/
 addpath ~/VCMatlab/Alternative_Complexity/
 
 
-images = dir('path/to/jpg/files');
+img = imread('/path/to/image/');
 
+%% Visual Complexity
 
+lc = luminance_complexity(img); % luminance complexity
+cc = colorfulness(img); % color complexity
+ed = edge_density(img); % edge density 
+ff = freqfactor(img); % feature complexity control: frequency factor
+[ah av irv] =arrangement(img); % asymmetry horizontal, asymmetry vertical, irregularity of object arrangement
+[~, num_reg]=color_harmony(img,10); % design complexity control: number of regions
 
+% the number of objects can be calculated using the ipython notebook or using pre-trained yolov4
 
-N = size(images,1);
+%% Alternative Complexity 
+% Corchs et al. (2014) complexity measures, m6 is equal to edge density, m7 is jpeg file size
+[m1, m2, m3, m4] = complexity1_4(img);
+m5 = freqfactor(img);
+m9 = colorfulness(img);
+m10 = numofcolors(img);
+[m11, m8]=color_harmony(img,10);
+oc = objectcomplexity(objects(k,:)); % object complexity measure used in Shin et al. (2021)
+[fc, se] = clutter(img); % clutter, feature congestion and subband entropy
 
-diag_domv=zeros(N,1); 
-rotv=zeros(N,1);
-vphdv=zeros(N,1);
-hphdv=zeros(N,1);
-horizontalv=zeros(N,1);
-verticalv=zeros(N,1);
-size_difv=zeros(N,1);
-col_difv=zeros(N,1);
-text_difv=zeros(N,1);
-brightnessv=zeros(N,1);
-saturationv=zeros(N,1);
-contrastv=zeros(N,1);
-clarityv=zeros(N,1);
-warmthv=zeros(N,1);
-fcv=zeros(N,1);
-sev=zeros(N,1);
-lcv=zeros(N,1);
-ccv=zeros(N,1);
-edv=zeros(N,1);
-%ocv=zeros(N,1);
-
-m1v = zeros(1,N);
-m2v = zeros(1,N);
-m3v = zeros(1,N);
-m4v = zeros(1,N);
-m5v = zeros(1,N);
-m8v = zeros(1,N);
-m9v = zeros(1,N);
-m10v = zeros(1,N);
-m11v = zeros(1,N);
-
-sdv = zeros(N,1);
-
-for k = 1:N
-    
-    im = strcat('path/to/images/',images(k).name);
-    img = imread(im);
-
-%% composition
-% segmentation
-
-sdv(k) = std(double(img(:)));
-
-
-if sdv(k)<10
-   continue 
-end
-
-
-%[regs, ~, ~] = segmentation(img,10); % input RGB image
-
-
-
-regs = superpixels(img,10);
 
 %visual saliency calculation
 
+regs = superpixels(img,10); 
 [vis_sal] = vsalience(img); %input same RGB image
-
 
 % determine salient region
 saliency_region = zeros(max(max(regs)),1);
@@ -89,8 +51,7 @@ yCentroids = centroids(2:2:end);
 m = find(saliency_region==max(saliency_region));
 center_salient = [xCentroids(m) yCentroids(m)];
 
-
-
+%% Photography Variables developed based on the photography constructs as used in Zhang et al. 2017, Zhang and Luo 2018
 %diagonal dominance 
 
 diag_dom = diagonaldominance(img, center_salient);
@@ -125,86 +86,10 @@ text_dif = texturedifference(img,A);
 
 [brightness, saturation, contrast, clarity, warmth] = color_photography(img);
 
-%% Visual Complexity
-option1 = imread('Option 1.png');
-option2 = imread('Option 2.png');
-option3 = imread('Option 3.png');
-option4 = imread('Option 4.png');
-img = option4;
 
 
 
-[fc, se] = clutter(img); % clutter, feature congestion and subband entropy
 
-lc = luminance_complexity(img); % luminance complexity
-cc = color_complexity(img); % color complexity
-ed = edge_density(img); % edge density 
-
-
-
-oc = objectcomplexity(objects(k,:));
-
-%% Alternative Complexity 
-% Corchs et al. complexity measures, m6 is equal to edge density, m7 is
-% jpeg file size
-[m1, m2, m3, m4] = complexity1_4(img);
-m5 = freqfactor(img);
-m9 = colorfulness(img);
-m10 = numofcolors(img);
-[m11, m8]=color_harmony(img,10);
-
-
-diag_domv(k)=diag_dom;
-rotv(k)=rot;
-vphdv(k)=vphd;
-hphdv(k)=hphd;
-horizontalv(k)=horizontal;
-verticalv(k)=vertical;
-size_difv(k)=size_dif;
-col_difv(k)=col_dif;
-text_difv(k)=text_dif;
-brightnessv(k)=brightness;
-saturationv(k)=saturation;
-contrastv(k)=contrast;
-clarityv(k)=clarity;
-warmthv(k)=warmth;
-fcv(k)=fc;
-sev(k)=se;
-lcv(k)=lc;
-ccv(k)=cc;
-edv(k)=ed;
-ocv(k)=oc;
-    
-
-
-
-ahv =zeros(N,1);
-avv = zeros(N,1);
-irv = zeros(N,1);
-
-for k = 1:N % to continue later
-    im = strcat('Instagram/NewData/',images(k).name);
-    img = imread(im); 
-    if numel(size(img))<3
-        continue
-    end
-    
-    if lcv2(k)==0 | sdv(k)<10
-        continue 
-    end
-    [ahv(k) avv(k) irv(k)] =arrangement(img);
-    if mod(k,1000)==0
-        fprintf('%d\n',k)
-    end
-end
-m8= m8v;
-m11 = m11v
-
-all_image_features = table(diag_domv,rotv,vphdv,hphdv,horizontalv,verticalv,size_difv,col_difv,text_difv,brightnessv,saturationv,contrastv,clarityv,warmthv,lcv,lcv2,ccv,edv,ocv,ahv,avv,irv,m1,m2,m3,m4,m5,m8,m9,m10,m11,CEv',SCv',indicator);
-image_names = struct2table(images);
-image_features = [all_image_features image_names.name];
-
-writetable(image_features,'image_features.xlsx')
 
 
 
