12f65c7ea4e2d45999371bc0be5e21928bfe92a0
Gyys1992 <65665662+Gyys1992@users.noreply.github.com>
2022-12-29 12:53:00 -0500

    Update object.m

--- Complexity/object.m
+++ Complexity/object.m
@@ -1,6 +1,4 @@
 function num_objects = object(img,detector)
-images = dir('P:\Champion\Champion of Images\Data2\Images\*.jpg');
-img = imread(append(images(1).folder,'\',images(4).name));
 img = preprocess(detector,img);
 img = im2single(img);
 [bboxes,scores,labels] = detect(detector,img,'DetectionPreprocessing','none');
